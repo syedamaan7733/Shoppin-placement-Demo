c1eb873029c5cc5f33d46b33abe3462640f796ee
syedamaan7733 <codegeek7733@gmail.com>
2024-12-08 20:15:38 +0530

    Updated Icon

--- components/Hero.js
+++ components/Hero.js
@@ -1,31 +1,31 @@
 import { CiMicrophoneOn, CiSearch } from "react-icons/ci";
 import { SlSocialGoogle } from "react-icons/sl";
-import "./index.css";
+import styles from "./index.module.css";
 export const Hero = () => {
   const languages = ["हिन्दी", "বাংলা", "తెలుగు", "मराठी", "मराठी"];
   return (
-    <div className="landing-cont">
+    <div className={styles.landing_cont}>
       <img
-        className="logo"
+        className={styles.logo}
         src="https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
       />
-      <div className="InputName">
-        <CiSearch style={{ fontSize: "20px" }} className="search" />
-        <input type="text" className="logo" placeholder="" />
-        <CiMicrophoneOn style={{ fontSize: "20px" }} className="mic" />
-        <SlSocialGoogle style={{ fontSize: "20px" }} className="lens" />
+      <div className={styles.InputName}>
+        <CiSearch style={{ fontSize: "20px" }} className={styles.search} />
+        <input type="text" className={styles.logo} placeholder="" />
+        <CiMicrophoneOn style={{ fontSize: "20px" }} className={styles.mic} />
+        <SlSocialGoogle style={{ fontSize: "20px" }} className={styles.lens} />
       </div>
       <div style={{ display: "flex", gap: "1em" }}>
-        <button className="buttons">Google Search</button>
-        <button className="buttons">I'm Feeling Lucky</button>
+        <button className={styles.buttons}>Google Search</button>
+        <button className={styles.buttons}>I'm Feeling Lucky</button>
       </div>
       {/* Language Links */}
-      <div className="lang-link">
+      <div className={styles.lang_link}>
         <p>Google offered in: </p>
 
         <div style={{ display: "flex", gap: "1em" }}>
           {languages.map((links) => {
-            return <a className="links">{links}</a>;
+            return <a className={styles.links}>{links}</a>;
           })}
         </div>
       </div>
